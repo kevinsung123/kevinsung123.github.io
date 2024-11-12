0cad38beeb823cefe6a43de95eaf2dc2b0fefa31
kevinsung123 <kevinsung123@naver.com>
2024-11-12 17:11:12 +0900

    docs: update

--- git_commit.sh
+++ git_commit.sh
@@ -2,11 +2,12 @@
 ## step 1 . step into git blog folder
 #cd /d/github/kevinsung123.github.io
 
-# check pwd
+# step1. git status
+git status 
 
 ## step2. read from stdin for commit messgae
-read -p "Write commit message for push : " com_msg
-echo "${com_msg}"
+read  -p "Write commit message for push : " com_msg
+echo "commit message : ${com_msg}"
 
 ## step 3. git add all files
 git add ./*
@@ -18,4 +19,4 @@ git commit -m "${com_msg}"
 git push
 
 ## step6. git log
-#git log --pretty=format:"%h -%an,  %cd (%ar) : %s" --graph
+git log --pretty=format:"%h -%an,  %cd (%ar) : %s" --graph
